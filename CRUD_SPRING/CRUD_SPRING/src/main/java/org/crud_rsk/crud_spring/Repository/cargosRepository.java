9fd4044c54c8582cb07f134f4df53553b33b3fa5
gastoonEXE <164912557+gastoonEXE@users.noreply.github.com>
2025-09-02 10:10:22 -0300

    se crea el proyecto react

--- CRUD_SPRING/CRUD_SPRING/src/main/java/org/crud_rsk/crud_spring/Repository/cargosRepository.java
+++
@@ -1,9 +0,0 @@
-package org.crud_rsk.crud_spring.Repository;
-
-import org.crud_rsk.crud_spring.entity.cargos;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface cargosRepository extends JpaRepository<cargos, Integer> {
-}
-
--- CRUD_SPRING/CRUD_SPRING/src/main/java/org/crud_rsk/crud_spring/Repository/rolesRepository.java
+++
@@ -1,9 +0,0 @@
-package org.crud_rsk.crud_spring.Repository;
-
-import org.crud_rsk.crud_spring.entity.roles;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface rolesRepository extends JpaRepository<roles, Integer> {
-}
